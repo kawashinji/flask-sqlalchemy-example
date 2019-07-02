6b64bcffe257dd68976406b2fe946029860d8a4d
Shinji Kawaguchi <shinji.kawaguchi@clouto.io>
2019-07-02 17:29:54 +0900

    Add dotenv

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+import random, string
 from flask import Flask
+from functools import reduce
+
 from app import redisClient
 from app import mysqlClient
 from db import init_db
 import app.models
-import random, string
-from functools import reduce
 
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.config.from_object('config.Config')
